36423f38c09bd3c7d35d1cb5c2c53d58b3a80180
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-13 07:29:00 +0200

    Add project files.

---
+++ Lekcja7.Zad.10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Pobieranie długości boków od użytkownika
+		Console.WriteLine("Podaj długości trzech boków trójkąta:");
+		int a = Convert.ToInt32(Console.ReadLine());
+		int b = Convert.ToInt32(Console.ReadLine());
+		int c = Convert.ToInt32(Console.ReadLine());
+
+		// Sprawdzanie czy z podanych długości można zbudować trójkąt
+		if (a + b > c && a + c > b && b + c > a)
+		{
+			Console.WriteLine("Można zbudować trójkąt");
+		}
+		else
+		{
+			Console.WriteLine("Nie można zbudować trójkąta");
+		}
+	}
+}
